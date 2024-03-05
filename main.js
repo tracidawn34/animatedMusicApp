8c52be025d30637f45cb96590bdcc6230410cf4b
justin beeler <tmcfall.prsvr@gmail.com>
2024-03-05 12:31:51 -0500

    changed and added css added js

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
-//const bird = new Audio('sounds/bird.wav')
-//const button1 = document.querySelector('#birdButton')
-//let count = 0
+const bird = new Audio('sounds/bird.wav')
+const bubble = new Audio('sounds/bubble.wav')
+const chimeUp = new Audio('sounds/chime-up.wav')
+const drumKick = new Audio('sounds/drum_kick.wav')
+const nordDrum = new Audio('sounds/nord-drum.wav')
+const slideWhistle = new Audio('sounds/slide-whistle.wav')
 
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const button3 = document.getElementById('button3')
+const button4 = document.getElementById('button4')
+const button5 = document.getElementById('button5')
+const button6 = document.getElementById('button6')
 
-//function play(){
-    //if(count >5){
-//count = 0
-   // }if(count === 5 && button1.pushed)
-   // bird.play()
-//} 
-const circle1 = document.querySelector('.item1')
-const whistle = new Audio('sounds/slide-whistle.wav')
-
-circle1.addEventListener('click', ()=>{
-if(circle1.clicked)
-whistle.play()
+button1.addEventListener('click', ()=>{
+    //console.log('click')
+    //if(button1.clicked)
+bird.currentTime = 0
+    bird.play()
 })
+
+button2.addEventListener('click', ()=>{
+    //if(button2.clicked)
+   bubble.currentTime = 0
+    bubble.play()
+})
+
+button3.addEventListener('click', ()=>{
+   chimeUp.currentTime = 0
+    chimeUp.play()
+})
+
+button4.addEventListener('click', ()=>{
+    drumKick.currentTime = 0
+    drumKick.play()
+})
+
+button5.addEventListener('click', ()=>{
+    nordDrum.currentTime = 0
+    nordDrum.play()
+})
+
+button6.addEventListener('click', ()=>{
+    slideWhistle.currentTime = 0
+    slideWhistle.play()
+})
+
+const makeBubbles = ()=>{
+const bubbles = document.createElement('div')
+viewPoint.appendChild(bubbles)
+bubbles.style.backgroundColor = "yellow"
+bubbles.style.animation = "jump 1s ease"
+
+}
